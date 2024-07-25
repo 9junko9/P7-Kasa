b37753b14123e9368d8f60ef6d0c5c414dc2d0d9
9junko9 <jennifer.rivolier@gmail.com>
2024-07-26 00:53:53 +0200

    aboutPage dynamique

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,16 +1,35 @@
 import { DescriptionPanel } from "../Components/DescriptionPanel";
-import ApartmentBanner from "../Components/ApartmentBanner";
+import bannerAbout from "../assets/Images/imageapropos.png";
+
 import "./About.scss";
 
 function About() {
   return (
     <>
-      <ApartmentBanner />
+      <div className="banner__about">
+        <img
+          src={bannerAbout}
+          alt="paysage de montagne"
+          className="banner__about__image"
+        />
+      </div>
       <div className="about__container">
-        <DescriptionPanel />
-        <DescriptionPanel />
-        <DescriptionPanel />
-        <DescriptionPanel />
+        <DescriptionPanel
+          title="Fiabilité"
+          content="à remplir avec les bonnes informations"
+        />
+        <DescriptionPanel
+          title="Respect"
+          content="à remplir avec les bonnes informations"
+        />
+        <DescriptionPanel
+          title="Service"
+          content="à remplir avec les bonnes informations"
+        />
+        <DescriptionPanel
+          title="Sécurité"
+          content="à remplir avec les bonnes informations"
+        />
       </div>
     </>
   );
