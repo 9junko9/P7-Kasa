bd72c6ae34303aa98523c910316ccb5541867ae6
9junko9 <jennifer.rivolier@gmail.com>
2024-07-26 00:17:42 +0200

    anim descriptions chevrons up and down

--- src/Components/DescriptionPanel.jsx
+++ src/Components/DescriptionPanel.jsx
@@ -1,13 +1,27 @@
 import "../Components/DescriptionPanel.scss";
+import { useState } from "react";
 
 export function DescriptionPanel(props) {
+  const [isContentVisible, setIsContentVisible] = useState(false);
+
+  const toggleContentVisibility = () => {
+    setIsContentVisible(!isContentVisible);
+  };
+
+  const contentClass = `description_content ${
+    isContentVisible ? "visible" : "hidden"
+  }`;
+  const chevronClass = `fas ${
+    isContentVisible ? "fa-chevron-up" : "fa-chevron-down"
+  }`;
+
   return (
     <div className="description__panel">
-      <p className="description_header">
+      <p className="description_header" onClick={toggleContentVisibility}>
         <span>{props.title}</span>
-        <i className="fa-solid fa-chevron-up"></i>
+        <i className={chevronClass}></i>
       </p>
-      <p className="description_content">{props.content}</p>
+      <p className={contentClass}>{props.content}</p>
     </div>
   );
 }
