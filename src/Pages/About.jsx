d0a48ff758cb6cb5815eda4cc6438fb1c69ae4e9
9junko9 <jennifer.rivolier@gmail.com>
2024-07-29 11:04:21 +0200

    final commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -15,22 +15,22 @@ function About() {
       <div className="about__container">
         <DescriptionPanel
           title="Fiabilité"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus.
+          content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements et toutes les informations sont régulièrement vérifiées par nos équipes.
 "
         />
         <DescriptionPanel
           title="Respect"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus.
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoireou de perturbationdu voisinage entraînera une exclusion de notre plateforme.
 "
         />
         <DescriptionPanel
           title="Service"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus.
+          content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.
 "
         />
         <DescriptionPanel
           title="Sécurité"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna. Morbi a bibendum metus.
+          content="La sécurité est la priorité de Kasa.Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'aux locataires,cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
 "
         />
       </div>
